af2a496ed20b5079cfaf8d8a2d1f48c9bb98f87d
seanmx96 <seanmx96@users.noreply.github.com>
2020-05-12 20:36:45 -0500

    wip

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -7,22 +7,26 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" >X</button>
+      <button className="button"
+      // onClick={() => props.removeFeature(props.key)}
+      >X</button>
       {props.feature.name}
     </li>
   );
 };
 
 // it might be a good idea to leave the individial feature components as functional components
-const mapStateToProps = state => {
-  console.log('AddedFeature.js mSTP is running...', { state });
+// const mapStateToProps = state => {
+//   console.log('AddedFeature.js mSTP is running...', { state });
 
-  return {
-    feature: state.car.features
-  }
-};
+//   return {
+//     feature: state.car.features
+//   }
+// };
 
-export default connect(
-  mapStateToProps,
-{})
-(AddedFeature);
+// export default connect(
+//   mapStateToProps,
+// { removeFeature })
+// (AddedFeature);
+
+export default AddedFeature;
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
 import { addFeature } from '../actions/featureActions';
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" 
+      onClick={() => addFeature(props.feature)}
+      >Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+
+// const mapStateToProps = state => {
+//   console.log('AddedFeature.js mSTP is running...', { state });
+
+//   // state.additionalFeatures.map(additionalFeature => {
+//   //     if (additionalFeature.id === action.payload.id) {
+//   //       return null;
+//   //     }
+//   //   }
+//   //   )
+
+//   return {
+//     feature: state.car.features
+//   }
+// };
+
+// export default connect(
+//   mapStateToProps, {addFeature})(AdditionalFeature);
 
 export default AdditionalFeature;
